3084dfc32eb1c2bd469f87e763f6afcf92859903
GB5026 <D@isuke10>
2019-01-14 14:40:39 +0100

    add change reason

--- base/forms.py
+++ base/forms.py
@@ -1,14 +1,16 @@
 from django import forms
-from django_select2.forms import Select2TagWidget
-from taggit.models import Tag
 
 
 class BaseForm(forms.ModelForm):
-    tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all(), widget=Select2TagWidget, required=False)
+    chg_reason = forms.CharField(label="Change reason", required=False)
 
     @classmethod
     def add_fields(cls, extra_fields):
         return ['name', 'status', 'tags', 'description', 'documentation'] + extra_fields + ['comment']
+
+    def save(self, commit=True):
+        self.instance.changeReason = self.cleaned_data['chg_reason']
+        return super().save(commit)
 
     class Meta:
         fields = '__all__'
